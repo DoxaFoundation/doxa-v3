e7272411d9fbfa9ddb9a509b25da20029b399953
Rahees <ahdrahees.dev@gmail.com>
2024-05-29 23:35:59 +0530

    add: ICRC3 for USDx and Decimals 6

--- scripts/deploy-local-usdx.sh
+++ scripts/deploy-local-usdx.sh
@@ -10,31 +10,41 @@ export MINTER_ACCOUNT=$(dfx canister id stablecoin_minter)
 
 TOKEN_NAME="Digital Dollar"
 TOKEN_SYMBOL="USDx"
+Decimals=6
 
 PRE_MINTED_TOKENS=0
 
-# Fee is 0.001 USDx
-TRANSFER_FEE=100_000
+# Fee is 0.01 USDx
+TRANSFER_FEE=10_000
 
 TRIGGER_THRESHOLD=2000
 NUM_OF_BLOCK_TO_ARCHIVE=1000
-CYCLE_FOR_ARCHIVE_CREATION=10000_000_000_000
+CYCLE_FOR_ARCHIVE_CREATION=10_000_000_000_000
 FEATURE_FLAGS=true
 
 dfx deploy usdx_ledger  --argument "(variant {Init = 
 record {
+     decimals = opt ${Decimals};
      token_symbol = \"${TOKEN_SYMBOL}\";
      token_name = \"${TOKEN_NAME}\";
      minting_account = record { owner = principal \"${MINTER_ACCOUNT}\" };
      transfer_fee = ${TRANSFER_FEE};
      metadata = vec {};
      feature_flags = opt record{icrc2 = ${FEATURE_FLAGS}};
-     initial_balances = vec { record { record { owner = principal \"${MINTER_ACCOUNT}\"; }; ${PRE_MINTED_TOKENS}; }; };
+     initial_balances = vec {};
+     maximum_number_of_accounts = null;
+     accounts_overflow_trim_quantity = null;
+     fee_collector_account = null;
+     max_memo_length = opt 80;
      archive_options = record {
          num_blocks_to_archive = ${NUM_OF_BLOCK_TO_ARCHIVE};
          trigger_threshold = ${TRIGGER_THRESHOLD};
          controller_id = principal \"${ARCHIVE_CONTROLLER}\";
          cycles_for_archive_creation = opt ${CYCLE_FOR_ARCHIVE_CREATION};
+         max_transactions_per_response = null;
+         more_controller_ids = null;
+         max_message_size_bytes = null;
+         node_max_memory_size_bytes = null;
      };
  }
 })"
