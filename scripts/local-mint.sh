cd3984fb5ea2d5d84a4f445203716fa6a86bd09d
vishnu vishwakarma <vv083150@gmail.com>
2024-11-18 15:03:39 +0530

    typo

--- scripts/local-mint.sh
+++ scripts/local-mint.sh
@@ -1,8 +1,12 @@
+# Store principals in variables
+export DEFAULT_ACCOUNT=$(dfx identity get-principal --identity default)
+export DOXA_ACCOUNT=$(dfx identity get-principal --identity doxa)
+
 # USDx Balance of default idenity
-dfx canister call usdx_ledger icrc1_balance_of '(record {owner= principal "5g24m-kxyrd-yb7wl-up5k6-4egww-miul7-gajat-e2d7i-mdpc7-6dduf-eae"})' --identity default
+dfx canister call usdx_ledger icrc1_balance_of "(record {owner= principal \"$DEFAULT_ACCOUNT\"})" --identity default
 
 # ckUSDC Balance of default idenity
-dfx canister call ckusdc_ledger icrc1_balance_of '(record {owner= principal "5g24m-kxyrd-yb7wl-up5k6-4egww-miul7-gajat-e2d7i-mdpc7-6dduf-eae"})' --identity default
+dfx canister call ckusdc_ledger icrc1_balance_of "(record {owner= principal \"$DEFAULT_ACCOUNT\"})" --identity default
 
 # Get (ckUSDC) Reserve Account of USDx 
 dfx canister call stablecoin_minter get_ckusdc_reserve_account_of '(record {token= variant {USDx}})'
@@ -10,9 +14,9 @@ dfx canister call stablecoin_minter get_ckusdc_reserve_account_of '(record {toke
 # Transfer ckUSDC from default identity to Reserve Account of USDx
 dfx canister call ckusdc_ledger icrc1_transfer '(record{ to=record {owner = principal "iyn2n-liaaa-aaaak-qddta-cai"; subaccount= opt blob "\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\01";} ; amount=9_000_000;})' --identity default
 
-# dfx canister call ckusdc_ledger icrc1_transfer '(record{ to=record {owner = principal "5g24m-kxyrd-yb7wl-up5k6-4egww-miul7-gajat-e2d7i-mdpc7-6dduf-eae"; subaccount= opt blob "\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00";} ; amount=9_000_000;})' --identity default
+# dfx canister call ckusdc_ledger icrc1_transfer "(record{ to=record {owner = principal \"$DEFAULT_ACCOUNT\"; subaccount= opt blob \"\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\";} ; amount=9_000_000;})" --identity default
 
-# dfx canister call usdx_ledger icrc1_transfer '(record{ to=record {owner = principal "5g24m-kxyrd-yb7wl-up5k6-4egww-miul7-gajat-e2d7i-mdpc7-6dduf-eae"; subaccount= opt vec{0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 1}}; amount = 9_000_000;})' --identity default
+# dfx canister call usdx_ledger icrc1_transfer "(record{ to=record {owner = principal \"$DEFAULT_ACCOUNT\"; subaccount= opt vec{0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 1}}; amount = 9_000_000;})" --identity default
 
 # Get ckUSDC Transaction
 dfx canister call ckusdc_ledger get_transactions '( record { start =0; length=3 })'
