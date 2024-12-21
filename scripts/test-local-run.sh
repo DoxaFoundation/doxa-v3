30729347956cba1c24f32cf041a830524ccd9180
vishnu vishwakarma <vv083150@gmail.com>
2024-12-21 16:49:10 +0530

    wip added log files

--- scripts/test-local-run.sh
+++ scripts/test-local-run.sh
@@ -2,28 +2,36 @@
 
 no_of_test_canisters=20
 
+# Create logs directory if it doesn't exist
+mkdir -p logs
+
 cd src/test
 
 for i in $(seq 0 $(($no_of_test_canisters - 1))); do
-    echo "############################################"
-    echo "Running test_canister_$((i+1))..."
-   
-    dfx canister call test_canister_$((i+1)) test
-
-    echo
-done
-
-for i in $(seq 0 $(($no_of_test_canisters - 1))); do
-    echo "############################################"
-    echo "Displaying Stake Details and Stake Metric for test_canister_$((i+1))..."
+    canister_num=$((i+1))
+    log_file="../../logs/test_canister_${canister_num}.log"
     
-    echo "Stake  Details"
-    dfx canister call test_canister_$((i+1)) getStakeDetails
-    echo
-    echo "Stake Metric"
-      dfx canister call test_canister_$((i+1)) getStakeMetric
-
-    echo
+    echo "############################################" > "$log_file"
+    echo "Running test_canister_${canister_num}..." >> "$log_file"
+    echo >> "$log_file"
+    
+    echo "Test Results:" >> "$log_file" 
+    dfx canister call test_canister_${canister_num} test >> "$log_file" 2>&1
+    echo >> "$log_file"
+    
+    echo "Stake Details:" >> "$log_file"
+    dfx canister call test_canister_${canister_num} getStakeDetails >> "$log_file" 2>&1
+    echo >> "$log_file"
+    
+    echo "Stake Metrics:" >> "$log_file"
+    dfx canister call test_canister_${canister_num} getStakeMetric >> "$log_file" 2>&1
+    echo >> "$log_file"
+    
+    echo "Pool Data:" >> "$log_file"
+    dfx canister call test_canister_${canister_num} getPoolInfo >> "$log_file" 2>&1
+    echo >> "$log_file"
+    
+    echo "Log file created for test_canister_${canister_num}"
 done
 
 cd ../../
