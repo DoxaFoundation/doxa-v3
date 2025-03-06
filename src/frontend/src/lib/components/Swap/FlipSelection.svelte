f067d36ef7a9fc177dfe85533f3f66c896e1b1a4
Rahees <ahdrahees.dev@gmail.com>
2025-03-06 14:57:40 +0530

    feat: UI for swap token selection and flip selection

---
+++ src/frontend/src/lib/components/Swap/FlipSelection.svelte
@@ -0,0 +1,44 @@
+<script>
+	import { ChevronsDownUpIcon, ChevronsUpDownIcon } from 'lucide-svelte';
+
+	let { from = $bindable(), to = $bindable() } = $props();
+
+	let animation = $state(false);
+
+	const switchTokens = () => {
+		const temp = from;
+		from = to;
+		to = temp;
+		animation = !animation;
+	};
+</script>
+
+<div class="flex justify-center items-center relative h-0">
+	<button
+		class="icon-wrapper absolute z-10 border p-2 rounded-full hover:bg-gray-200 bg-white"
+		class:open={animation}
+		onclick={switchTokens}
+	>
+		<ChevronsDownUpIcon class="size-8 text-black transition-all duration-300 ease-out" />
+	</button>
+</div>
+
+<style>
+	.icon-wrapper {
+		transition: transform 300ms ease-out;
+	}
+
+	/* hover over increase size */
+	.icon-wrapper:hover {
+		transform: scale(1.08);
+	}
+
+	/*  flip*/
+	.icon-wrapper.open {
+		transform: rotateX(180deg);
+	}
+
+	.icon-wrapper.open:hover {
+		transform: rotateX(180deg) scale(1.08);
+	}
+</style>
