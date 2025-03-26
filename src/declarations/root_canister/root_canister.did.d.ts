e904cc0941e291027c919e6804b392e0dee8e2a0
Rahees <ahdrahees.dev@gmail.com>
2025-03-26 11:19:39 +0530

    feat: add root_canister declaration

--- src/declarations/root_canister/root_canister.did.d.ts
+++ src/declarations/root_canister/root_canister.did.d.ts
@@ -2,6 +2,13 @@ import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 
-export interface _SERVICE {}
+export type EmailPermission = { 'Deny' : null } |
+  { 'Allow' : string };
+export type Result = { 'ok' : null } |
+  { 'err' : string };
+export interface _SERVICE {
+  'get_email_permission' : ActorMethod<[], [] | [EmailPermission]>,
+  'insert_email' : ActorMethod<[[] | [string]], Result>,
+}
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
