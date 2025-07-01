4e66970884c13b187658d0dc92c68f9042a9f28a
Rahees <ahdrahees.dev@gmail.com>
2025-07-01 22:19:44 +0530

    backend for emergency halting and blocking bad actos

--- src/declarations/root_canister/root_canister.did.d.ts
+++ src/declarations/root_canister/root_canister.did.d.ts
@@ -2,15 +2,63 @@ import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 
+export type Canister = { 'all' : null } |
+  { 'ckusdc_pool' : null } |
+  { 'dusd_index' : null } |
+  { 'frontend' : null } |
+  { 'dusd_ledger' : null } |
+  { 'root_canister' : null } |
+  { 'stablecoin_minter' : null } |
+  { 'staking_canister' : null } |
+  { 'utility_canister' : null };
+export interface CanisterStatusResult {
+  'status' : { 'stopped' : null } |
+    { 'stopping' : null } |
+    { 'running' : null },
+  'memory_size' : bigint,
+  'cycles' : bigint,
+  'settings' : DefiniteCanisterSettings,
+  'query_stats' : {
+    'response_payload_bytes_total' : bigint,
+    'num_instructions_total' : bigint,
+    'num_calls_total' : bigint,
+    'request_payload_bytes_total' : bigint,
+  },
+  'idle_cycles_burned_per_day' : bigint,
+  'module_hash' : [] | [Uint8Array | number[]],
+  'reserved_cycles' : bigint,
+}
+export interface DefiniteCanisterSettings {
+  'freezing_threshold' : bigint,
+  'controllers' : Array<Principal>,
+  'reserved_cycles_limit' : bigint,
+  'log_visibility' : LogVisibility,
+  'wasm_memory_limit' : bigint,
+  'memory_allocation' : bigint,
+  'compute_allocation' : bigint,
+}
 export type EmailPermission = { 'Deny' : null } |
   { 'Allow' : string };
+export type LogVisibility = { 'controllers' : null } |
+  { 'public' : null };
 export type Result = { 'ok' : null } |
+  { 'err' : string };
+export type Result_1 = { 'ok' : Array<CanisterStatusResult> } |
+  { 'err' : string };
+export type Result_2 = { 'ok' : Array<[string, bigint]> } |
   { 'err' : string };
 export interface _SERVICE {
   'accept_risk_warning' : ActorMethod<[], Result>,
+  'add_bad_actor' : ActorMethod<[Principal], Result>,
+  'canister_balances' : ActorMethod<[Array<Canister>], Result_2>,
+  'canisters_status' : ActorMethod<[Array<Canister>], Result_1>,
   'get_email_permission' : ActorMethod<[], [] | [EmailPermission]>,
   'get_risk_warning_agreement' : ActorMethod<[], [] | [boolean]>,
   'insert_email' : ActorMethod<[[] | [string]], Result>,
+  'is_bad_actor' : ActorMethod<[], boolean>,
+  'remove_bad_actor' : ActorMethod<[Principal], Result>,
+  'start_canisters' : ActorMethod<[Array<Canister>], Result>,
+  'stop_canisters' : ActorMethod<[Array<Canister>], Result>,
 }
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
--- src/declarations/root_canister/root_canister.did.js
+++ src/declarations/root_canister/root_canister.did.js
@@ -1,11 +1,65 @@
 export const idlFactory = ({ IDL }) => {
   const Result = IDL.Variant({ 'ok' : IDL.Null, 'err' : IDL.Text });
+  const Canister = IDL.Variant({
+    'all' : IDL.Null,
+    'ckusdc_pool' : IDL.Null,
+    'dusd_index' : IDL.Null,
+    'frontend' : IDL.Null,
+    'dusd_ledger' : IDL.Null,
+    'root_canister' : IDL.Null,
+    'stablecoin_minter' : IDL.Null,
+    'staking_canister' : IDL.Null,
+    'utility_canister' : IDL.Null,
+  });
+  const Result_2 = IDL.Variant({
+    'ok' : IDL.Vec(IDL.Tuple(IDL.Text, IDL.Nat)),
+    'err' : IDL.Text,
+  });
+  const LogVisibility = IDL.Variant({
+    'controllers' : IDL.Null,
+    'public' : IDL.Null,
+  });
+  const DefiniteCanisterSettings = IDL.Record({
+    'freezing_threshold' : IDL.Nat,
+    'controllers' : IDL.Vec(IDL.Principal),
+    'reserved_cycles_limit' : IDL.Nat,
+    'log_visibility' : LogVisibility,
+    'wasm_memory_limit' : IDL.Nat,
+    'memory_allocation' : IDL.Nat,
+    'compute_allocation' : IDL.Nat,
+  });
+  const CanisterStatusResult = IDL.Record({
+    'status' : IDL.Variant({
+      'stopped' : IDL.Null,
+      'stopping' : IDL.Null,
+      'running' : IDL.Null,
+    }),
+    'memory_size' : IDL.Nat,
+    'cycles' : IDL.Nat,
+    'settings' : DefiniteCanisterSettings,
+    'query_stats' : IDL.Record({
+      'response_payload_bytes_total' : IDL.Nat,
+      'num_instructions_total' : IDL.Nat,
+      'num_calls_total' : IDL.Nat,
+      'request_payload_bytes_total' : IDL.Nat,
+    }),
+    'idle_cycles_burned_per_day' : IDL.Nat,
+    'module_hash' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+    'reserved_cycles' : IDL.Nat,
+  });
+  const Result_1 = IDL.Variant({
+    'ok' : IDL.Vec(CanisterStatusResult),
+    'err' : IDL.Text,
+  });
   const EmailPermission = IDL.Variant({
     'Deny' : IDL.Null,
     'Allow' : IDL.Text,
   });
   return IDL.Service({
     'accept_risk_warning' : IDL.Func([], [Result], []),
+    'add_bad_actor' : IDL.Func([IDL.Principal], [Result], []),
+    'canister_balances' : IDL.Func([IDL.Vec(Canister)], [Result_2], []),
+    'canisters_status' : IDL.Func([IDL.Vec(Canister)], [Result_1], []),
     'get_email_permission' : IDL.Func(
         [],
         [IDL.Opt(EmailPermission)],
@@ -13,6 +67,10 @@ export const idlFactory = ({ IDL }) => {
       ),
     'get_risk_warning_agreement' : IDL.Func([], [IDL.Opt(IDL.Bool)], ['query']),
     'insert_email' : IDL.Func([IDL.Opt(IDL.Text)], [Result], []),
+    'is_bad_actor' : IDL.Func([], [IDL.Bool], ['query']),
+    'remove_bad_actor' : IDL.Func([IDL.Principal], [Result], []),
+    'start_canisters' : IDL.Func([IDL.Vec(Canister)], [Result], []),
+    'stop_canisters' : IDL.Func([IDL.Vec(Canister)], [Result], []),
   });
 };
 export const init = ({ IDL }) => { return []; };
